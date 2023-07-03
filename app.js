ed789537430a85c543dd4a51351588a31baa108d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-07-03 18:07:49 +0300

    store pieces SVGs in pieces.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDiplay = document.querySelector("#player");
 const infoDiplay = document.querySelector("#info-display");
+
+const startPieces = [
+    
+]
