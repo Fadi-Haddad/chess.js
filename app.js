46ee8d34835baabbae0e79d6bf372a89d5f2a80e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-07-03 18:22:42 +0300

    Create initial pieces formation and store it in an array

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDiplay = document.querySelector("#player");
 const infoDiplay = document.querySelector("#info-display");
+const width = 8;
 
 const startPieces = [
-    
+    rook, knight, bishop , queen , king, bishop, knight, rook,
+    pawn, pawn,pawn, pawn,pawn, pawn,pawn, pawn,
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    '','','','','','','','',
+    pawn, pawn,pawn, pawn,pawn, pawn,pawn, pawn,
+    rook, knight, bishop , queen , king, bishop, knight, rook,
 ]
