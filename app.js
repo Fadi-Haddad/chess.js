e08fdc6bc6138e585958d7aab77385cc5e3dd3e6
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-07-03 18:40:22 +0300

    Add styling to board and squares

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ function createBoard(){
     startPieces.forEach((startPiece) => {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.classList.add('beige');
         gameBoard.append(square);
     })
 }
+createBoard();
